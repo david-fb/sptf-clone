d73a4d1b29908132aead2d58c05785dec4aea64e
David Basto <davidbasto01@gmail.com>
2024-11-22 00:20:36 -0500

    add: search component styles

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import spotifyApi from '../controllers/SpotifyApi';
 import useStore from "../config/store.jsx"
 import SearchIcon from '../assets/icons/SearchIcon';
@@ -7,7 +7,7 @@ import ExploreIcon from "../assets/icons/ExploreIcon";
 const Search = () => {
   const [query, setQuery] = useState("");
   const setPlayList = useStore((state) => state.setPlayList);
-
+  const inputRef = useRef(null);
 
   const onSearch = (query) => {
     spotifyApi
@@ -20,36 +20,42 @@ const Search = () => {
       })
       .then((response) => {
         console.log("Respuesta:", response.data);
-        //Canciones 
         const tracks = response.data.tracks?.items || [];
         console.log("Resultados:", tracks);
         setPlayList(tracks);
       })
       .catch((err) => console.error("Error al buscar:", err));
   };
-  
-
 
   const handleSubmit = (e) => {
     e.preventDefault();
     onSearch(query);
   };
 
+  const handleIconClick = () => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex items-center bg-tertiary-dark rounded-full px-4 py-2 w-full h-full max-w-lg mx-auto shadow-md focus-within:ring-2 focus-within:ring-white"
+      className="group flex items-center cursor-pointer bg-tertiary-dark rounded-full px-4 py-2 w-full h-full max-w-lg mx-auto shadow-md focus-within:ring-2 focus-within:ring-white text-neutral transition-colors hover:bg-quaternary-dark border-[1px] border-transparent hover:border-[1px] hover:border-neutral"
     >
-      <SearchIcon />
+      <button onClick={handleIconClick} className="transition-colors group-hover:text-white group-focus-within:text-white group-focus-within:cursor-default">
+        <SearchIcon />
+      </button>
       <input
+        ref={inputRef}
         type="text"
         placeholder="¿Qué quieres reproducir?"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="flex-1 bg-transparent text-white placeholder-gray-400 outline-none px-2"
+        className="flex-1 bg-transparent text-white placeholder-neutral outline-none px-2 cursor-pointer group-focus-within:cursor-text"
       />
-      <div className="border-l pl-2">
-        <button className="h-fit">
+      <div className="border-l pl-2 h-6">
+        <button className="h-fit cursor-pointer transition-all hover:scale-105 hover:text-white">
           <ExploreIcon />
         </button>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ export default {
         'primary-dark': '#000000',   // Color principal
         'secondary-dark': '#121212', // Color secundario
         'tertiary-dark': '#1f1f1f',  // Color terciario
+        'quaternary-dark': '#2f2f2f', // Color cuaternario
+        'neutral': '#b3b3b3',
       },
     },
   },
